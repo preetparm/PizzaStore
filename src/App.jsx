7529f569ff5b75f276b35e0447435fa9d4e792b2
PreetParm <parmpreetsingh4@gmail.com>
2024-10-15 20:00:56 -0400

    footer Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import './App.css'
 import { Itemlist } from './Components/Additems/Itemlist'
 import MainCusins  from './Components/mainCusins/MainCusins'
+import Footer from './Components/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,9 @@ function App() {
       </div>
       <div className='maincusin'>
         <MainCusins />
+      </div>
+      <div>
+        <Footer/>
       </div>
     </div>
   );
---
+++ src/Components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Footer.module.css'; // Import the CSS module
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; Made With 💘By  Parampreet Singh.</p>
+    </footer>
+  );
+};
+
+export default Footer;
